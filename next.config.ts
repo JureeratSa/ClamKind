3f7c5ab1f2963a4df02961cdcb92616c59bc3fba
Jureerat Saenchom <jureerat.dev@gmail.com>
2026-05-23 21:32:13 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: "public", // แหล่งเก็บไฟล์ Service Worker ที่จะถูกสร้างขึ้นมา
-  disable: process.env.NODE_ENV === "development", // ปิดการทำงานในโหมด Development จะได้ไม่ติด Cache เวลาแก้โค้ด
+  dest: "public", // Service Worker ที่จะถูกสร้างขึ้นมา
+  disable: process.env.NODE_ENV === "development",
   register: true,
   workboxOptions: {
     skipWaiting: true,
